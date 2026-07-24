fa36dece8613503ce1b64ee2c7207eb94510166b
Alya Saad <3lya.sa3d@gmail.com>
2026-07-23 22:28:16 -0400

    Add files via upload

---
+++ nav-links.ts
@@ -0,0 +1,9 @@
+export const navLinks = [
+  { href: '/', label: 'Home' },
+  { href: '/background', label: 'Background' },
+  { href: '/norms', label: 'Norms' },
+  { href: '/structures', label: 'Structures' },
+  { href: '/change', label: 'Social Change' },
+  { href: '/theory', label: 'Theory & Questions' },
+  { href: '/references', label: 'References' },
+]
